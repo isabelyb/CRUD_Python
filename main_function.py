53fa56d6b7e611f65c8be2b5cb947b4527c85bf6
isabelyb <isabelcristinayepesb@gmail.com>
2021-06-13 20:17:10 -0500

    Fin CRUD

--- main_function.py
+++ main_function.py
@@ -13,9 +13,31 @@ def create_client(client_name):
 
 
 # Mostrar a los Clientes
-def list_clients():
+def read_clients():
     global clients
     print(clients)
+
+
+# Actualizar clientes
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        print('Client is not in clients list')
+
+
+# Borrar clientes
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', '')
+    else:
+        print('Client is not in clients list')
+
+
 
 
 # Función auxiliar para añadir coma despues de cada cliente
@@ -33,19 +55,40 @@ def _print_welcome():
     print('What would you like to do today?')
     print()
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
     print()
+
+
+def _get_client_name():
+    return input('What is the client name? ')
 
 if __name__ == '__main__':
     _print_welcome()
 
     command = input()
+    command = command.upper() # Lo vuelve mayúscula
 
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name()
         create_client(client_name)
-        list_clients()
+        read_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name? ')
+        update_client(client_name, updated_client_name)
+        read_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        read_clients()
     else:
         print('Invalid command')
+
+
+
+
+def funcion():
+    '''Documentar función'''
+    pass
+
